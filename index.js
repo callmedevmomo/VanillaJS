035dd89b6c58eb2feadb5830d13cb624e865050b
momo <callmedevmomo@gmail.com>
2019-03-13 21:40:04 +0900

    vanilla-js test
    array object

---
+++ index.js
@@ -0,0 +1,46 @@
+console.log("Im Working. Im JS. Im Beautiful. Im worth it");
+let a = 221;
+let b = a - 5;
+a = 4;
+console.log(b, a);
+/*
+//String
+const what = "momo";
+
+console.log(what);
+
+//Boolean binary true1 false0
+const wat = true;
+
+//Number
+const wat2 = 666;
+
+//float
+const wat3 = 55.5;
+*/
+
+const something = "something";
+const daysOfWeek = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun", something];
+console.log(daysOfWeek);
+// console.log(monday, tue, wed, thu, fri);
+
+const momoInfo = ["momo", "27", true, "Seoul"];
+
+console.log(momoInfo);
+
+const momoInfo2 = {
+  name: "momo",
+  age: "27",
+  male: true,
+  city: "Seoul",
+  favMovies: ["Along the gods", "LOTR", "Oldboy"],
+  favFood: [
+    { name: "Kimch", fatty: false },
+    { name: "Cheese burger", fatty: true }
+  ]
+};
+console.log(momoInfo2.city);
+momoInfo2.city = "Busan";
+
+console.log(momoInfo2.city);
+console.log(momoInfo2.favFood[0].fatty);
